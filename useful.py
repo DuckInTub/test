a71404caa223ec15bf78d7a5ef317d255dcdd334
icebl0804 <icebl0804@gmail.com>
2021-11-03 15:48:13 +0100

    Added useful

---
+++ useful.py
@@ -0,0 +1,8 @@
+
+def factorial(n):
+    return 1 if n == 0 else factorial(n - 1) * n
+
+if __name__ == "__main__":
+    print(factorial(0) == 1)
+    print(factorial(3) == 6)
+    print(factorial(4) == 24)
